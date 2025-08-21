03d2b0ebac83ed225c48f6f847395d1c83fecbd5
Rintaro Yamauchi <yamauchi.r.b970@m.isct.ac.jp>
2025-08-21 16:33:34 +0900

    avg-res修正

--- mxfold2/utils/avg-res.py
+++ mxfold2/utils/avg-res.py
@@ -1,5 +1,28 @@
+# import pandas as pd
+# import sys
+
+# df = pd.read_csv(sys.argv[1], names=("name", "length", "elapsed_time", "score", "TP", "TN", "FP", "FN", "SEN", "PPV", "F", "MCC"))
+# print(df[["length", "elapsed_time", "SEN", "PPV", "F"]].describe())
+
 import pandas as pd
 import sys
 
-df = pd.read_csv(sys.argv[1], names=("name", "length", "elapsed_time", "score", "TP", "TN", "FP", "FN", "SEN", "PPV", "F", "MCC"))
-print(df[["length", "elapsed_time", "SEN", "PPV", "F"]].describe())
+
+pd.set_option("display.max_columns", None)  # 列を省略しない
+pd.set_option("display.width", None)       # 横幅も制限しない
+
+df = pd.read_csv(sys.argv[1], names=(
+    "name", "length", "elapsed_time", "score",
+    "TP", "TN", "FP", "FN", "SEN", "PPV", "F", "MCC",
+    "mse", "r2", "corr"  # Multitask の場合だけ出てくる
+))
+
+# 共通で欲しい列
+cols = ["length", "elapsed_time", "SEN", "PPV", "F", "MCC"]
+
+# もし SHAPE 指標が含まれていれば追加
+for extra in ["mse", "r2", "corr"]:
+    if extra in df.columns:
+        cols.append(extra)
+
+print(df[cols].describe())
